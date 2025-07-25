1a4c04fdec6e4b784219c7d4fd22f046f50b13cb
cristian <cvo6372@gmail.com>
2025-07-25 09:52:37 -0500

    first commit with base configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CountryList } from './modules/countries/pages/country-list/country-list';
+import { LogsList } from './modules/logs/pages/logs-list/logs-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'countries',
+        pathMatch: 'full',
+    },
+    {
+        path: 'countries',
+        component: CountryList,
+    },
+    {
+        path: 'logs',
+        component: LogsList,
+    },
+];
